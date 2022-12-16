3f7f8c9d4f2ed1c5aab9d8edd463c49b15ee0c5c
Scott <yusufs3435@gmail.com>
2022-12-16 22:37:07 +0300

    2022-12-16-22-36-54

--- 07-Javascript/Instruction/assets/js/44-event-handling-practise.js
+++ 07-Javascript/Instruction/assets/js/44-event-handling-practise.js
@@ -5,18 +5,49 @@ const btnAverage = document.querySelector("#btnAverage");
 
 
 btnAverage.addEventListener("click", ()=>{
-
+    const average = calculateAverage(txtScore1.value,txtScore2.value,txtScore3.value);
+    if(!average){
+        alert("Please enter valid scores");
+        return;
+    }
+    const letter = convertScoreToLetter(average);
+    alert(`Average: ${average} Score: ${letter}`);
 });
 
 const calculateAverage = (num1, num2, num3) => {
-    // ortalama işlemi
-    return 15;
+    if(!isScoreValid(num1) || !isScoreValid(num2) || !isScoreValid(num3)){
+        return false;
+    }
+    const result = (Number(num1) + Number(num2) + Number(num3))/3;
+    return result;
 }
 
 
 const convertScoreToLetter = (num) => {
-    // harfe çevirme işlemi
-    return "A";
+    if(!isScoreValid(num)){
+        return false;
+    }
+    let letter = "";
+    if(num>=90 && num<=100){
+        letter = "A";
+    }
+    else if(num>=80 && num<90){
+        letter = "B";
+    }
+    else if(num>=70 && num<80){
+        letter = "C";
+    }
+    else if(num>=50 && num<70){
+        letter = "D";
+    }
+    else if(num>=0 && num<50){
+        letter = "F";
+    }
+    return letter;
 }
 
+
+const isScoreValid = (score) => {
+    return ( score || score===0) && !isNaN(score) && score<=100 && score>=0;
+}
 
