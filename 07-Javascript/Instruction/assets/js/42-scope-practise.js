7cb01c3988cc708a3c4d63decd54251754f69a65
Scott <yusufs3435@gmail.com>
2022-12-16 15:37:31 +0300

    2022-12-16-15-37-22

--- 07-Javascript/Instruction/assets/js/42-scope-practise.js
+++ 07-Javascript/Instruction/assets/js/42-scope-practise.js
@@ -5,9 +5,13 @@ const btnStart = document.querySelector(".btn-start");
 const numEl = document.querySelector("#txtNumber");
 const labelEl = document.querySelector("#lblResult");
 
+const minRandomNumber = 1;
+const maxRAndomNumber = 100;
+const totalShot = 5;
+
 
 const start = () => {
-    randomNumber = generateRandomNumber(1,100);
+    randomNumber = generateRandomNumber(minRandomNumber,maxRAndomNumber);
     btnGuess.style.display = "inline";
     btnStart.innerHTML = "Reset Game";
     labelEl.innerHTML = "";
@@ -24,6 +28,11 @@ const reset = () => {
 const guess = () =>{
     let num = Number(numEl.value);
     num = num || 0;
+    /*
+    if(!num){
+        num = 0;
+    }
+    */
 
     if(num === randomNumber){
         labelEl.innerHTML = "Congrats! You guessed the number";
---
+++ 07-Javascript/Instruction/assets/js/44-event-handling-practise.js
@@ -0,0 +1,22 @@
+const txtScore1 = document.querySelector("#txtScore1");
+const txtScore2 = document.querySelector("#txtScore2");
+const txtScore3 = document.querySelector("#txtScore3");
+const btnAverage = document.querySelector("#btnAverage");
+
+
+btnAverage.addEventListener("click", ()=>{
+
+});
+
+const calculateAverage = (num1, num2, num3) => {
+    // ortalama işlemi
+    return 15;
+}
+
+
+const convertScoreToLetter = (num) => {
+    // harfe çevirme işlemi
+    return "A";
+}
+
+
