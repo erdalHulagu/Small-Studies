6469919053c855b77369716a6790776a10cac15d
Scott <yusufs3435@gmail.com>
2022-12-20 23:59:16 +0300

    2022-12-20-23-59-04

--- 07-Javascript/Instruction/assets/js/67-dom-practise.js
+++ 07-Javascript/Instruction/assets/js/67-dom-practise.js
@@ -1,16 +1,42 @@
 import { students } from "../data/students.js";
 
+const tblStudentsTbody = document.querySelector("#tblStudents tbody");
+
+
+/* EVENTS */
+document.getElementById("btnShowLowScores").addEventListener("click", ()=>{
+    const lastTDs = tblStudentsTbody.querySelectorAll("tr td:last-child");
+    console.log(lastTDs);
+
+    lastTDs.forEach( (td, index)=> {
+        if(td.innerText<50){
+            //td.style.backgroundColor = "red";
+            tblStudentsTbody.querySelector(`tr:nth-child(${index+1})`).style.backgroundColor = "red";
+        }
+    })
+
+
+});
+
+/* EVENTS */
+
 const loadData = () => {
-    let strHtml = "";
+  let strHtml = "";
 
-    students.forEach( (student, index)=> {
-        strHtml +=  `<tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
+  students.forEach((student, index) => {
+    strHtml += `<tr>
+        <th scope="row">${index+1}</th>
+        <td>${student.name}</td>
+        <td>${student.point}</td>
       </tr>`;
-    } )
+  });
+
+  tblStudentsTbody.innerHTML = strHtml;
+};
 
 
 
-}
+
+
+
+loadData();
