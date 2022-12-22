8cbd73ed2386099282ed5d248ec4d7757ef10241
eylulSolmaz <eylul.techproed@gmail.com>
2022-12-22 19:53:47 +0300

    js-adv-prc 2.gun

--- 07-Javascript/Practise/Adv-Practice/1.gun/tasKagitMakas/2.yol/app.js
+++ 07-Javascript/Practise/Adv-Practice/1.gun/tasKagitMakas/2.yol/app.js
@@ -13,3 +13,61 @@ Append sayfada seçtiğimiz bir alanın sonuna veri eklememizi sağlar.
 
 
  */
+
+const kullaniciSecimiEl = document.createElement("h2");
+const sanalSecimEl = document.createElement("h2");
+const sonucEl = document.createElement("h2");
+const containerEl = document.getElementById("container");
+
+containerEl.append(kullaniciSecimiEl, sanalSecimEl, sonucEl);
+
+const secenekler = ["tas", "kagit", "makas"];
+
+let kullaniciSecimi;
+let sanalSecim;
+
+const handleClick = (e) => {
+  kullaniciSecimi = e.target.id;
+  kullaniciSecimiEl.innerHTML = `Senin seçimin:${kullaniciSecimi}`;
+  generateSanalSecim();
+  getSonuc();
+};
+
+const generateSanalSecim = () => {
+  sanalSecim = secenekler[Math.floor(Math.random() * secenekler.length)];
+  sanalSecimEl.innerHTML = `Bilgisayarın seçimin:${sanalSecim}`;
+};
+
+/* Butonları ekleyelim */
+for (let i = 0; i < secenekler.length; i++) {
+  const buton = document.createElement("button");
+  buton.setAttribute("id", secenekler[i]);
+  buton.innerHTML = secenekler[i];
+  buton.addEventListener("click", handleClick);
+  containerEl.appendChild(buton);
+}
+
+/* kimin kazandığını belirlediğimiz fonksiyon */
+/*  Taş makası, makas kağıdı, kâğıt da 
+taşı yener. Eğer oyuncular aynı durumu seçerse 
+oyun berabere biter.  */
+
+const getSonuc = () => {
+  switch (kullaniciSecimi + sanalSecim) {
+    case "makaskagit":
+    case "tasmakas":
+    case "kagittas":
+      sonucEl.innerHTML = "SEN KAZANDIN";
+      break;
+    case "kagitmakas":
+    case "makastas":
+    case "taskagit":
+      sonucEl.innerHTML = "KAYBETTİN!";
+      break;
+    case "kagitkagit":
+    case "makasmakas":
+    case "tastas":
+      sonucEl.innerHTML = "BERABERESİNİZ!";
+      break;
+  }
+};
