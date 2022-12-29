d04c3716837c40419bde74b6754c6410fbcc5d6b
eylulSolmaz <eylul.techproed@gmail.com>
2022-12-29 17:09:39 +0300

    js-adv-5.gun hzrlk

---
+++ 07-Javascript/Practise/Adv-Practice/5.gun/bilet/app.js
@@ -0,0 +1,13 @@
+/* indexOf() metodu, var olan bir dizi içerisinde 
+aradığımız bir elemanın index numarasını geriye döndürür.  */
+
+/* 
+JSON.stringify :JavaScripti JSON biçimine dönüştürmek 
+için kullanılır.
+
+JSON.parse : JSON nesnelerini JavaScript’e dönüştürmek 
+için kullanılır. 
+*/
+
+/* selectedIndex özelliği, bir açılır listede seçilen 
+seçeneğin indexini ayarlar veya döndürür. */
---
+++ 07-Javascript/Practise/Adv-Practice/5.gun/hafızaOyunu/app.js
@@ -0,0 +1,23 @@
+/* DOMContentLoaded –
+ HTML yüklenip, işlenip, DOM tamamen inşa edildiğinde. */
+
+/*  
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+  }
+}
+
+EDIT: Updating to ES6 / ECMAScript 2015
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+*/
