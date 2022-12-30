aa5b60c0c1b73412f18eb94761d447b3af9720f8
Scott <yusufs3435@gmail.com>
2022-12-31 00:28:15 +0300

    2022-12-31-00-28-01

---
+++ 09-Webpack/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module:{
+    rules:[
+        {
+            test:/\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        },
+        {
+            test:/\.s(a|c)ss$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }
+    ]  
+  }
+};
