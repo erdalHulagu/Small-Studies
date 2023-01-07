e60c06f526fc333b7526102c95eec4459528a799
Scott <yusufs3435@gmail.com>
2023-01-07 22:34:07 +0300

    2023-01-07-22-33-55

--- 10-React/Instruction/resume/src/components/main/about/about.js
+++ 10-React/Instruction/resume/src/components/main/about/about.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import SectionHeader from '../section-header/section-header';
+import "./about.css";
+import Intro from './intro/intro';
 
 const About = () => {
   return (
     <section id="about" className="about">
-
+        <SectionHeader/>
+        <Intro/>
     </section>
   )
 }
