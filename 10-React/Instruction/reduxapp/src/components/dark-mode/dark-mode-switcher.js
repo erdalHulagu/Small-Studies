bd75a9cc9305e1d982a888bc1bf1ab94930b476a
Scott <yusufs3435@gmail.com>
2023-01-21 21:59:07 +0300

    2023-01-21-21-58-53

--- 10-React/Instruction/reduxapp/src/components/dark-mode/dark-mode-switcher.js
+++ 10-React/Instruction/reduxapp/src/components/dark-mode/dark-mode-switcher.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Form } from "react-bootstrap";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { setDarkMode } from "../../store/slices/theme-slice";
 
 const DarkModeSwitcher = () => {
-  const darkMode = useSelector( (state)=> state.theme.darkMode  )
+  const darkMode = useSelector((state) => state.theme.darkMode);
+  const dispatch = useDispatch();
 
   return (
     <div>
-      <Form.Check type="switch" id="dmSwitch" label="Dark Mode"  checked={darkMode} />
+      <Form.Check
+        type="switch"
+        id="dmSwitch"
+        label="Dark Mode"
+        checked={darkMode}
+        onChange={ ()=>  dispatch(setDarkMode(!darkMode))}
+      />
     </div>
   );
 };
