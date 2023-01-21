ba6234f1b89a1d98025791547380ac3f57ab41eb
Scott <yusufs3435@gmail.com>
2023-01-21 21:34:53 +0300

    2023-01-21-21-34-40

---
+++ 10-React/Instruction/reduxapp/src/store/slices/theme-slice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+export const themeSlice = createSlice({
+    name: "theme",
+    initialState:{
+        darkMode: false
+    },
+    reducers:{
+        setDarkMode: (state, action) => {
+            state.darkMode = action.payload;
+        }
+    }
+});
+
+export const { setDarkMode } = themeSlice.actions;
+export default themeSlice.reducer;
